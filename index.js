2be33d62c4813f088065248744aca930c63857fb
gtx46 <gtx46@ubuntu.ubuntu-domain>
2019-02-05 14:39:56 +0100

    attributes

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 class NativePasswordInput extends HTMLElement {
     constructor() {
         super() ;
-        const placeholder = this.getAttribute('placeholder') ;
-        const name = this.getAttribute('name') ;
         this.attachShadow({ mode: "open" });
+        
         this.shadowRoot.innerHTML = 
         /*html*/`
         <style>
             
             #password {
-                width: 30%;
-                border: 2px solid #aaa;
-                border-radius: 4px;
+                width: 100%;
+                
+                font-size: 14px;
+                border-bottom: 1px solid rgba(0, 0, 0, .12);
+                
                 margin: 8px 0;
                 outline: none;
                 padding: 8px;
@@ -25,14 +26,14 @@ class NativePasswordInput extends HTMLElement {
                 box-shadow: 0 0 8px 0 dodgerBlue;
             }
             
-            .putWithIcon  {
+            .inputWithIcon  {
                 position: relative;
             }
             
             #showPassword {
                 position: absolute;
-                left: 2px;
-                top: 12px;
+                left: -2px ;
+                top: 5px;
                 padding: 9px 8px;
                 color: #aaa;
                 transition: 0.3s;
@@ -41,18 +42,52 @@ class NativePasswordInput extends HTMLElement {
             }
             
             .inputWithIcon #password : focus + i {
-                color: dodgerBlue;
+                color: rgba(0, 0, 0, .38);
             }
         </style>
             <div class=${'inputWithIcon'}>
-                <input id=${'password'} placeholder="${placeholder}" type=${'password'} name="${name}">
-                    <i aria-hidden="true">
+                <i aria-hidden="true">
                     <img id=${'showPassword'} src=${'./node_modules/native-password-input/img/closed.svg'}>
-                    </i>
+                </i>
+                <input id=${'password'} value="" placeholder="" type=${'password'} name="">
             </div>
          ` ;
     }
+    set passwordValue(value) {
+        this.setAttribute('password-value', value);
+    };
+    get passwordValue() {
+        return this._passwordValue ;
+    };
+
+    set placeholder(value) {
+        this.setAttribute('placeholder', value);
+    };
+    get placeholder() {
+        return this._placeholder ;
+    };
+
+    set name(value) {
+        this.setAttribute('name', value);
+    };
+    get name() {
+        return this._name ;
+    };
+    
+
     connectedCallback() {
+        this._placeholder = this.getAttribute('placeholder') ;
+        if(!this._placeholder) {this._placeholder= " ";}
+        this._name = this.getAttribute('name') ;
+        this._passwordValue = this.getAttribute('password-value') ;
+        this._backgroundColor = this.getAttribute('background-color-att') ;
+        this._border = this.getAttribute('border-att') ;
+       
+        this.shadowRoot.querySelector('input').addEventListener('focusout', (event) => {
+            document.getElementById('password').placeholder = this.shadowRoot.querySelector('input').value ;
+            this._passwordValue = this.shadowRoot.querySelector('input').value ;
+         });
+        
          this.shadowRoot.getElementById('showPassword').addEventListener(
             'click', (event) => {
                 var password = this.shadowRoot.getElementById('password');
@@ -68,9 +103,32 @@ class NativePasswordInput extends HTMLElement {
                 }
             }
         );
-
+        this.render() ;
     }
 
+    render() {
+        this.shadowRoot.querySelector('input').name = this._name ;
+        this.shadowRoot.querySelector('input').placeholder = this._placeholder ;
+        this.shadowRoot.querySelector('input').value = this._passwordValue ;
+        this.shadowRoot.querySelector('input').style.backgroundColor = this._backgroundColor ;
+        this.shadowRoot.querySelector('input').style.border = this._border ;
+    }
+
+    static get obserevedAttributes() {
+        return ['placeholder', 'name', 'password-value'] ;
+    }
+
+    attributeChangedCallback(name, oldVal, newVal) {
+        if(name == 'placeholder')
+            this._placeholder = newVal ;
+        if(name == 'name')
+            this._name = newVal ;
+        if(name == 'password-value')
+            this._passwordValue = newVal ;
+
+        this.render() ;
+    }
 }
 
 window.customElements.define("native-password-input", NativePasswordInput);
+
